71045cf1415e4f1c8942cbae8afc60a24667ca58
Aswathi Unni <aswathi.u@tataelxsi.co.in>
2021-07-21 19:54:15 +0200

    Create stadiumTraffic.sql

---
+++ stadiumTraffic.sql
@@ -0,0 +1,53 @@
+/*
+Table: Stadium
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| visit_date    | date    |
+| people        | int     |
++---------------+---------+
+visit_date is the primary key for this table.
+Each row of this table contains the visit date and visit id to the stadium with the number of people during the visit.
+No two rows will have the same visit_date, and as the id increases, the dates increase as well.
+ 
+
+Write an SQL query to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
+
+Return the result table ordered by visit_date in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Stadium table:
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 1    | 2017-01-01 | 10        |
+| 2    | 2017-01-02 | 109       |
+| 3    | 2017-01-03 | 150       |
+| 4    | 2017-01-04 | 99        |
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
++------+------------+-----------+
+
+Result table:
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
+*/
+
+with temp as (
+select id, visit_date, people from Stadium where people >= 100)
+select distinct s1.*  from temp s1,
+temp s2,
+temp s3
+where (s1.id = s2.id - 1 and s1.id = s3.id - 2) or (s1.id = s2.id + 1 and s1.id = s3.id - 1) or (s1.id = s2.id + 2 and s1.id = s3.id + 1) order by visit_date;
